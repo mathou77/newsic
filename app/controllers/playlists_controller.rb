095185c5fa5d592fb827e115fc27ac6387b471d5
Pierre Gayno <pierre.gayno@gmail.com>
2026-06-03 10:33:21 +0200

    added the wipe cards, fixed the sound, populated the recap page

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,9 @@
 class PlaylistsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def vote
+    playlist = Playlist.find(params[:id])
+    playlist.update!(status: params[:status])
+    render json: { ok: true }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :suggestions, only: [:index, :show, :create] do
     member { get :recap }
-    resources :songs, only: [:show]
+    resources :playlists, only: [] do
+      member { patch :vote }
+    end
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
